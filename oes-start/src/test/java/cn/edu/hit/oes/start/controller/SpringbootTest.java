a6fb3c50aa8797feeef1b22725dfb53b0df80768
BeiyanLuansheng <941197279@qq.com>
2021-11-29 20:54:16 +0800

    spring boot

---
+++ oes-start/src/test/java/cn/edu/hit/oes/start/controller/SpringbootTest.java
@@ -0,0 +1,12 @@
+package cn.edu.hit.oes.start.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringbootTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
