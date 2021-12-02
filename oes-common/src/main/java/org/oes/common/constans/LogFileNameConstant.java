031f460849c0f132fbc606f63acc3182ad7c3b71
BeiyanLuansheng <941197279@qq.com>
2021-12-02 21:04:55 +0800

    logger

---
+++ oes-common/src/main/java/org/oes/common/constans/LogFileNameConstant.java
@@ -0,0 +1,6 @@
+package org.oes.common.constans;
+
+public class LogFileNameConstant {
+
+    public static final String OES = "oesLogger";
+}
--- oes-start/src/main/java/org/oes/start/controller/MainController.java
+++ oes-start/src/main/java/org/oes/start/controller/MainController.java
@@ -2,6 +2,9 @@ package org.oes.start.controller;
 
 import org.oes.biz.entity.Course;
 import org.oes.biz.service.CourseService;
+import org.oes.common.constans.LogFileNameConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +14,15 @@ import java.util.Date;
 @RestController
 public class MainController {
 
+    private static final Logger logger = LoggerFactory.getLogger(LogFileNameConstant.OES);
+
     @Resource
     CourseService courseService;
 
-    @GetMapping("/test")
-    public String getStr() {
-        return "Test";
+    @GetMapping("/log")
+    public String log() {
+        logger.error("Test logger in MainController");
+        return "log success";
     }
 
     @GetMapping("/course/create")
