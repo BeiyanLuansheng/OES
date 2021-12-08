b0fac40657873e83c2abb6dd8ad82e27a6952699
BeiyanLuansheng <941197279@qq.com>
2021-12-08 11:16:41 +0800

    不鉴权

--- oes-gateway/src/main/java/org/oes/gateway/controller/CourseController.java
+++ oes-gateway/src/main/java/org/oes/gateway/controller/CourseController.java
@@ -1,6 +1,6 @@
 package org.oes.gateway.controller;
 
-import org.apache.shiro.authz.annotation.RequiresPermissions;
+//import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.oes.biz.entity.Course;
 import org.oes.biz.service.CourseService;
 import org.oes.common.entity.OesResponse;
@@ -18,7 +18,7 @@ public class CourseController {
     @Resource
     private CourseService courseService;
 
-    @RequiresPermissions("course:add")
+//    @RequiresPermissions("course:add")
     public OesResponse addCourse(Course course) { // 必要时可以加 @Valid 校验参数
         this.courseService.createCourse(course);
         return new OesResponse().success();
