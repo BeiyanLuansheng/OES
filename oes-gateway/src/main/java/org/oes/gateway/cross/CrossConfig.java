3bc6563509dab9d21e28c058abd0a68e21c9c384
BeiyanLuansheng <941197279@qq.com>
2021-12-16 18:04:04 +0800

    跨域

---
+++ oes-gateway/src/main/java/org/oes/gateway/cross/CrossConfig.java
@@ -0,0 +1,23 @@
+package org.oes.gateway.cross;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域请求配置
+ * @author GavenLee
+ * @since 2020/10/20 9:55
+ */
+@Configuration
+public class CrossConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT","PATCH")
+                .maxAge(3600);
+    }
+}
