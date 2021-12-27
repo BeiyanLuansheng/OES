a5777ab8bd2fb8795bea2616f6cfe4da94788e67
BeiyanLuansheng <941197279@qq.com>
2021-12-27 14:27:29 +0800

    异常

--- oes-biz/src/main/java/org/oes/biz/handler/GlobalExceptionHandler.java
+++ oes-biz/src/main/java/org/oes/biz/handler/GlobalExceptionHandler.java
@@ -21,13 +21,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = Exception.class)
     public OesHttpResponse handleException(Exception e) {
-        logger.error("系统内部异常，异常信息", e);
-        return OesHttpResponse.getFailure("系统内部异常");
+        logger.error("系统未知异常，异常信息", e);
+        return OesHttpResponse.getFailure("系统未知异常");
     }
 
     @ExceptionHandler(value = OesException.class)
     public OesHttpResponse handleOesException(OesException e) {
-        logger.error("系统错误", e);
+        logger.error("系统内错误", e);
         return OesHttpResponse.getFailure(e.getMessage());
     }
 
